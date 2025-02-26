2e044a59b9b301b64573807745335a9400d744d2
sernamar <sernamar@protonmail.com>
2025-02-26 08:33:15 +0100

    Add support for `FastMoney` in conversion and rounding

--- src/dinero/rounding.clj
+++ src/dinero/rounding.clj
@@ -10,29 +10,31 @@
   "Rounds the given monetary amount"
   ([money]
    (let [minor-units (currency/get-minor-units (core/get-currency money))
-         decimal-places (or minor-units (BigDecimal/.scale (core/get-amount money)))
+         decimal-places (or minor-units (BigDecimal/.scale (bigdec (core/get-amount money))))
          rounding-mode (or core/*default-rounding-mode* :half-even)]
      (round money decimal-places rounding-mode)))
   ([money rounding-fn]
    (rounding-fn money))
   ([money decimal-places rounding-mode]
-   (let [amount (core/get-amount money)
+   (let [amount (bigdec (core/get-amount money))
          currency (core/get-currency money)
          rounding-mode-object (utils/keyword->rounding-mode rounding-mode)
          rounded (BigDecimal/.setScale ^BigDecimal amount ^int decimal-places ^RoundingMode rounding-mode-object)]
-     (if (core/money? money)
-       (core/money-of rounded currency)
-       (core/rounded-money-of rounded currency decimal-places rounding-mode)))))
+     (cond
+       (core/money? money) (core/money-of rounded currency)
+       (core/fast-money? money) (core/fast-money-of rounded currency)
+       :else (core/rounded-money-of rounded currency decimal-places rounding-mode)))))
 
 (defn chf-rounding-fn
   "Creates a rounding function for Swiss Francs."
   [money]
-  (let [amount (core/get-amount money)
+  (let [amount (bigdec (core/get-amount money))
         currency (core/get-currency money)
         scale 0
         rounded (BigDecimal/.multiply
                  (BigDecimal/.divide ^BigDecimal amount (bigdec 0.05) scale RoundingMode/HALF_UP)
                  (bigdec 0.05))]
-    (if (core/money? money)
-      (core/money-of rounded currency)
-      (core/rounded-money-of rounded currency 2 :half-up))))
+    (cond
+      (core/money? money) (core/money-of rounded currency)
+      (core/fast-money? money) (core/fast-money-of rounded currency)
+      :else (core/rounded-money-of rounded currency 2 :half-up))))
--- test/dinero/rounding_test.clj
+++ test/dinero/rounding_test.clj
@@ -11,18 +11,27 @@
       (t/is (= (core/money-of 1234.56 :eur) (sut/round money 2 :down)))
       (t/is (= (core/money-of 1235 :eur) (sut/round money 0 :up)))
       (t/is (= (core/money-of 1234 :eur) (sut/round money 0 :down)))))
-  (t/testing "Rounded money"
+  (t/testing "Rounded Money"
     (let [money(core/rounded-money-of 1234.5678 :eur)]
       (t/is (= (core/rounded-money-of 1234.57 :eur) (sut/round money)))
       (t/is (= (core/rounded-money-of 1234.57 :eur 2 :up) (sut/round money 2 :up)))
       (t/is (= (core/rounded-money-of 1234.57 :eur 2 :down) (sut/round money 2 :down)))
       (t/is (= (core/rounded-money-of 1235 :eur 0 :up) (sut/round money 0 :up)))
       (t/is (= (core/rounded-money-of 1234 :eur 0 :down) (sut/round money 0 :down)))))
+  (t/testing "Fast Money"
+    (let[money (core/fast-money-of 1234.5678 :eur)]
+      (t/is (= (core/fast-money-of 1234.57 :eur) (sut/round money)))
+      (t/is (= (core/fast-money-of 1234.57 :eur) (sut/round money 2 :up)))
+      (t/is (= (core/fast-money-of 1234.56 :eur) (sut/round money 2 :down)))
+      (t/is (= (core/fast-money-of 1235 :eur) (sut/round money 0 :up)))
+      (t/is (= (core/fast-money-of 1234 :eur) (sut/round money 0 :down)))))
   (t/testing "Currency with no minor units (`nil`)"
     (let [money (core/money-of 1.23 :xau)
-          rounded-money (core/rounded-money-of 1.23 :xau)]
+          rounded-money (core/rounded-money-of 1.23 :xau)
+          fast-money (core/fast-money-of 1.23 :xau)]
       (t/is (= 1.23M (core/get-amount (sut/round money))))
-      (t/is (= 1.23M (core/get-amount (sut/round rounded-money)))))))
+      (t/is (= 1.23M (core/get-amount (sut/round rounded-money))))
+      (t/is (= 1.23 (core/get-amount (sut/round fast-money)))))))
 
 (t/deftest round-chf
   (t/testing "Money"
@@ -38,7 +47,7 @@
       (t/is (= (core/money-of 1.05 :chf) (sut/round m4 sut/chf-rounding-fn)))
       (t/is (= (core/money-of 1.10 :chf) (sut/round m5 sut/chf-rounding-fn)))
       (t/is (= (core/money-of 1.10 :chf) (sut/round m6 sut/chf-rounding-fn)))))
-  (t/testing "Rounded money"
+  (t/testing "Rounded Money"
     (let [m1 (core/rounded-money-of 0.975 :chf)
           m2 (core/rounded-money-of 1.024 :chf)
           m3 (core/rounded-money-of 1.025 :chf)
@@ -50,4 +59,18 @@
       (t/is (= (core/rounded-money-of 1 :chf 2 :half-up) (sut/round m3 sut/chf-rounding-fn)))
       (t/is (= (core/rounded-money-of 1.05 :chf 2 :half-up) (sut/round m4 sut/chf-rounding-fn)))
       (t/is (= (core/rounded-money-of 1.10 :chf 2 :half-up) (sut/round m5 sut/chf-rounding-fn)))
-      (t/is (= (core/rounded-money-of 1.10 :chf 2 :half-up) (sut/round m6 sut/chf-rounding-fn))))))
+      (t/is (= (core/rounded-money-of 1.10 :chf 2 :half-up) (sut/round m6 sut/chf-rounding-fn)))))
+  (t/testing
+      "Fast Money"
+    (let [m1 (core/fast-money-of 0.975 :chf)
+          m2 (core/fast-money-of 1.024 :chf)
+          m3 (core/fast-money-of 1.025 :chf)
+          m4 (core/fast-money-of 1.074 :chf)
+          m5 (core/fast-money-of 1.075 :chf)
+          m6 (core/fast-money-of 1.124 :chf)]
+      (t/is (= (core/fast-money-of 1 :chf) (sut/round m1 sut/chf-rounding-fn)))
+      (t/is (= (core/fast-money-of 1 :chf) (sut/round m2 sut/chf-rounding-fn)))
+      (t/is (= (core/fast-money-of 1.05 :chf) (sut/round m3 sut/chf-rounding-fn)))
+      (t/is (= (core/fast-money-of 1.05 :chf) (sut/round m4 sut/chf-rounding-fn)))
+      (t/is (= (core/fast-money-of 1.10 :chf) (sut/round m5 sut/chf-rounding-fn)))
+      (t/is (= (core/fast-money-of 1.10 :chf) (sut/round m6 sut/chf-rounding-fn))))))
