bf757fc7713f6fd1d1aad221c63754e0b1b3e0fc
sernamar <sernamar@protonmail.com>
2025-02-28 10:12:36 +0100

    Update benchmark results

--- dev/arithmetic_operations_simulation.clj
+++ dev/arithmetic_operations_simulation.clj
@@ -2,41 +2,44 @@
   (:require [criterium.core :as criterium]
             [dinero.core :as core]))
 
-(def money-simulation
-  (let [addend (core/money-of 1234567.3444 :eur)
-        subtrahend (core/money-of 232323 :eur)]
-    (fn [money]
-      (-> money
-          (core/add addend)
-          (core/subtract subtrahend)
-          (core/multiply 3.4)
-          (core/divide 5.456)))))
+(let [addend (core/money-of 1234567.3444 :eur)
+      subtrahend (core/money-of 232323 :eur)]
+  (defn money-simulation
+    [money]
+    (-> money
+        (core/add addend)
+        (core/subtract subtrahend)
+        (core/multiply 3.4)
+        (core/divide 5.456))))
 
 (comment
   (criterium/bench
       (reduce (fn [acc _] (money-simulation acc)) (core/money-of 0 :eur) (range 1000000))))
 ;; Evaluation count : 60 in 60 samples of 1 calls.
-;;              Execution time mean : 2,324543 sec
-;;     Execution time std-deviation : 10,797871 ms
-;;    Execution time lower quantile : 2,311543 sec ( 2,5%)
-;;    Execution time upper quantile : 2,351981 sec (97,5%)
-;;                    Overhead used : 6,521613 ns
-
-;; Found 4 outliers in 60 samples (6,6667 %)
-;; 	low-severe	 4 (6,6667 %)
+;;              Execution time mean : 2,184454 sec
+;;     Execution time std-deviation : 10,434630 ms
+;;    Execution time lower quantile : 2,158174 sec ( 2,5%)
+;;    Execution time upper quantile : 2,203948 sec (97,5%)
+;;                    Overhead used : 6,970274 ns
+;;
+;; Found 9 outliers in 60 samples (15,0000 %)
+;; 	low-severe	 3 (5,0000 %)
+;; 	low-mild	 2 (3,3333 %)
+;; 	high-mild	 3 (5,0000 %)
+;; 	high-severe	 1 (1,6667 %)
 ;;  Variance from outliers : 1,6389 % Variance is slightly inflated by outliers
 
 ;; => {:amount 1657407.9625291828793774319066147859922178988326848249027237354085603112840466926070038910505836575875486381322957198443579766536964980544747081712062256809338521400778210116731517509727626459143968871595330739299610894941634241245136186770428015564202334630350194M, :currency :eur}
 
-(def rounded-money-simulation
-  (let [addend (core/rounded-money-of 1234567.3444 :eur 2 :half-even)
-        subtrahend (core/rounded-money-of 232323 :eur 2 :half-even)]
-    (fn [money]
-      (-> money
-          (core/add addend)
-          (core/subtract subtrahend)
-          (core/multiply 3.4)
-          (core/divide 5.456)))))
+(let [addend (core/rounded-money-of 1234567.3444 :eur 2 :half-even)
+      subtrahend (core/rounded-money-of 232323 :eur 2 :half-even)]
+  (defn rounded-money-simulation
+    [money]
+    (-> money
+        (core/add addend)
+        (core/subtract subtrahend)
+        (core/multiply 3.4)
+        (core/divide 5.456))))
 
 (comment
  (criterium/bench
@@ -55,28 +58,29 @@
 
 ;; => {:amount 1657407.95M, :currency :eur, :scale 2, :rounding-mode :half-even}
 
-(def fast-money-simulation
-  (let [addend (core/fast-money-of 1234567.3444 :eur)
-        subtrahend (core/fast-money-of 232323 :eur)]
-    (fn [money]
-      (-> money
-          (core/add addend)
-          (core/subtract subtrahend)
-          (core/multiply 3.4)
-          (core/divide 5.456)))))
+(let [addend (core/fast-money-of 1234567.3444 :eur)
+      subtrahend (core/fast-money-of 232323 :eur)]
+  (defn fast-money-simulation
+    [money]
+    (-> money
+        (core/add addend)
+        (core/subtract subtrahend)
+        (core/multiply 3.4)
+        (core/divide 5.456))))
 
 (comment
   (criterium/bench
       (reduce (fn [acc _] (fast-money-simulation acc)) (core/fast-money-of 0 :eur) (range 1000000))))
 ;; Evaluation count : 60 in 60 samples of 1 calls.
-;;              Execution time mean : 1,672895 sec
-;;     Execution time std-deviation : 7,729889 ms
-;;    Execution time lower quantile : 1,662126 sec ( 2,5%)
-;;    Execution time upper quantile : 1,689020 sec (97,5%)
-;;                    Overhead used : 6,521613 ns
-
-;; Found 2 outliers in 60 samples (3,3333 %)
-;; 	low-severe	 2 (3,3333 %)
-;;  Variance from outliers : 1,6389 % Variance is slightly inflated by outliers
+;;              Execution time mean : 1,338023 sec
+;;     Execution time std-deviation : 24,957735 ms
+;;    Execution time lower quantile : 1,314668 sec ( 2,5%)
+;;    Execution time upper quantile : 1,395095 sec (97,5%)
+;;                    Overhead used : 6,970274 ns
+;;
+;; Found 7 outliers in 60 samples (11,6667 %)
+;; 	low-severe	 5 (8,3333 %)
+;; 	low-mild	 2 (3,3333 %)
+;;  Variance from outliers : 7,8135 % Variance is slightly inflated by outliers
 
 ;; => {:amount 1657407.96252, :currency :eur}
