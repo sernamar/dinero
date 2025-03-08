de144289d40b7118ca7744f53e40d4c8a9f90048
sernamar <sernamar@protonmail.com>
2025-03-08 18:45:48 +0100

    Release version 0.2.1

--- build.clj
+++ build.clj
@@ -3,7 +3,7 @@
             [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'com.sernamar/dinero)
-(def version "0.2.0")
+(def version "0.2.1")
 
 (def target-dir "target/")
 (def class-dir (str target-dir "classes/"))
