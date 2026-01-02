c9c76cc21fa056df005ae60b6d934e8011c767bb
Sergio Navarro <sernamar@protonmail.com>
2026-01-02 11:17:48 +0100

    Update file headers with project name and author email

--- src/dinero/conversion/ecb.clj
+++ src/dinero/conversion/ecb.clj
@@ -1,10 +1,12 @@
-;; Copyright (c) 2025 Sergio Navarro
-;;
-;; This Source Code Form is subject to the terms of the Mozilla Public
-;; License, v. 2.0. If a copy of the MPL was not distributed with this
-;; file, You can obtain one at https://mozilla.org/MPL/2.0/.
-;;
-;; SPDX-License-Identifier: MPL-2.0.
+;;; dinero - A Clojure library for working with money
+;;;
+;;; Copyright (c) 2025 Sergio Navarro <sernamar@protonmail.com>
+;;;
+;;; This Source Code Form is subject to the terms of the Mozilla Public
+;;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;;; file, You can obtain one at https://mozilla.org/MPL/2.0/.
+;;;
+;;; SPDX-License-Identifier: MPL-2.0.
 
 (ns dinero.conversion.ecb
   (:require [clojure.string :as str]
